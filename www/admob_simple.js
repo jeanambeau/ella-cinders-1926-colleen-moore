08e6159305534095508d66de83652ce98b4a4adb
jeanambeau <ambeaujean@gmail.com>
2019-05-27 19:42:28 -0400

    Add existing file

--- www/admob_simple.js
+++ www/admob_simple.js
@@ -3,15 +3,15 @@ var admobid = {};
 // TODO: replace the following ad units with your own
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
-    banner: 'ca-app-pub-3940256099942544/6300978111',
-    interstitial: 'ca-app-pub-3940256099942544/1033173712',
-    rewardvideo: 'ca-app-pub-3940256099942544/5224354917',
+    banner: '',
+    interstitial: 'ca-app-pub-2041208520808887/4693860341',
+    rewardvideo: '',
   };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
-    banner: 'ca-app-pub-3940256099942544/2934735716',
-    interstitial: 'ca-app-pub-3940256099942544/4411468910',
-    rewardvideo: 'ca-app-pub-3940256099942544/1712485313',
+    banner: '',
+    interstitial: '',
+    rewardvideo: '',
   };
 } else {
   admobid = { // for Windows Phone, deprecated
