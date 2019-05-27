e170f9eb28e2c0d6501125090070dd4aacd3bf7b
jeanambeau <ambeaujean@gmail.com>
2019-05-27 19:21:18 -0400

    Add existing file

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -403,6 +403,14 @@ angular.module("ella_cinders_1926_colleen_moore.controllers", [])
 
 // TODO: ellaCtrl --|-- 
 .controller("ellaCtrl", function($ionicConfig,$scope,$rootScope,$state,$location,$ionicScrollDelegate,$ionicListDelegate,$http,$httpParamSerializer,$stateParams,$timeout,$interval,$ionicLoading,$ionicPopup,$ionicPopover,$ionicActionSheet,$ionicSlideBoxDelegate,$ionicHistory,ionicMaterialInk,ionicMaterialMotion,$window,$ionicModal,base64,md5,$document,$sce,$ionicGesture,$translate,tmhDynamicLocale){
+	// TODO: ellaCtrl --|-- showInterstitial
+	if (typeof AdMob !== "undefined"){
+		try{
+			AdMob.showInterstitial();
+		}catch(err){ 
+			//alert(err.message);
+		}
+	}
 	
 	$rootScope.headerExists = true;
 	$rootScope.ionWidth = $document[0].body.querySelector(".view-container").offsetWidth || 412;
